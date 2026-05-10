462e19ddea58b36addb71e5398c3edab6e862e38
fiyn <alienafis@gmail.com>
2026-05-11 02:14:53 +0800

    Worked on W7

---
+++ utils/TestDataHelper.js
@@ -0,0 +1,40 @@
+import fs from "fs";
+import path from "path";
+
+class TestDataHelper {
+    static loadJsonData(filename) {
+        const filePath = path.join(__dirname, "../test-data", filename);
+        const rawdata = fs.readFileSync(filePath, 'utf-8');
+        return JSON.parse(rawdata);
+    }
+
+    static getValidUser(userType = 'standard') {
+        const users = this.loadJsonData('users.json');
+        return users.validUsers[userType];
+    }
+
+    static getInvalidUser() {
+        const users = this.loadJsonData('users.json');
+        return users.getInvalidUser;
+    }
+
+    static getLockedUser() {
+        const users = this.loadJsonData('users.json');
+        return users.lockedUser;
+    }
+
+    static getProducts() {
+        const products = this.loadJsonData('products.json');
+        return products.testProducts;
+    }
+
+    static getRandomProduct() {
+        const products = this.getProducts();
+        return products[Math.floor(Math.random() * products.length)];
+    }
+
+    static getProductByName(name) {
+        const products = this.getProducts();
+        return products.find(p => p.name === name);
+    }
+}
